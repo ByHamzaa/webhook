c3f2d062ec3cf9c57ee5b0fdd257ffe797710672
CsYBot <67687653+CsYBot@users.noreply.github.com>
2021-01-27 18:37:44 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+$webhookurl = "Webhookurl";
+
+
+$timestamp = date("c", strtotime("now"));
+
+$json_data = json_encode([
+    "username" => "username",
+    "tts" => false,
+
+"content" => "message",
+
+    "embeds" => [
+        [
+            "title" => "Siteden Mesaj;",
+            "type" => "rich",
+            "description" => "Embed açıklama",
+            "url" => "https://www.csybot.cf",
+            "timestamp" => $timestamp,
+            "color" => hexdec( "3366ff" ),
+            "footer" => [
+                "text" => "©CsY",
+                "icon_url" => "https://images.discordapp.net/avatars/729389049881690143/9e698c392183c795708395e51bd4a2a2.png"
+            ],
+            "author" => [
+                "name" => "username"
+            ]
+        ]
+    ]
+], 
+
+
+JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+
+$ch = curl_init( $webhookurl );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+}
+?>
